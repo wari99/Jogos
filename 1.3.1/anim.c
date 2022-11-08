052142b7a4c47b0d837c11c9c3476f03fccdaaca
Mariana <57501545+wari99@users.noreply.github.com>
2022-11-08 12:39:22 -0300

    Update anim.c

--- 1.3.1/anim.c
+++ 1.3.1/anim.c
@@ -13,32 +13,32 @@ int main (int argc, char* args[])
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
     /* EXECUÇÃO */
-    
+    int v = 1;    
     int x = 40, y = 40;
     int z = 200, w = 110;
     
-    while (x < 200) {
+    while (1) {
         SDL_SetRenderDrawColor(ren, 0x8B, 0x45, 0x13,0x00);
         SDL_RenderClear(ren);
         
         filledCircleRGBA(ren, x,y,33, 0,0,0,255); //Borda Preta Alvo Cima
-        filledCircleRGBA(ren, z,w,33, 0,0,0,255); //Borda Preta Alvo Baixo
+        filledCircleRGBA(ren, z,w,33, 0,0,0,255); //Borda Preta Alvo Meio
 
         filledCircleRGBA(ren, x,y,30, 220,20,60,255); //Vermelho 30 Cima
         filledCircleRGBA(ren, x,y,20, 240,248,255,255); //Branco 20 Cima
         
-        filledCircleRGBA(ren, z,w,30, 220,20,60,255); //Vermelho 30 Baixo
-        filledCircleRGBA(ren, z,w,20, 240,248,255,255); //Branco 20 Baixo
+        filledCircleRGBA(ren, z,w,30, 220,20,60,255); //Vermelho 30 Meio
+        filledCircleRGBA(ren, z,w,20, 240,248,255,255); //Branco 20 Meio
 
 
         SDL_RenderPresent(ren);
 
         if(x % 2 == 0){
             filledCircleRGBA(ren, x, y,10, 220,20,60,255); //Vermelho Cima 10            
-            filledCircleRGBA(ren, z, w,10, 220,20,60,255); //Vermelho Baixo 10
+            filledCircleRGBA(ren, z, w,10, 220,20,60,255); //Vermelho Meio 10
             
             filledCircleRGBA(ren, x,y,4, 255,255,0,255); //Bullseye Cima
-            filledCircleRGBA(ren, z,w,4, 255,255,0,255); //Bullseye Baixo
+            filledCircleRGBA(ren, z,w,4, 255,255,0,255); //Bullseye Meio
 
                        
             SDL_RenderPresent(ren);         
@@ -49,27 +49,30 @@ int main (int argc, char* args[])
             filledCircleRGBA(ren, x,y, 20, 220,20,60,255); //Vermelho Cima 20
             filledCircleRGBA(ren, x,y, 10, 240,248,255,255); //Branco CIma 10
 
-            filledCircleRGBA(ren, z,w, 30, 240, 248,255,255);//Branco Baixo 30
-            filledCircleRGBA(ren, z,w, 20, 220,20,60,255); //Vermelho Baixo 20
-            filledCircleRGBA(ren, z,w, 10, 240,248,255,255); //Branco Baixo 10
+            filledCircleRGBA(ren, z,w, 30, 240, 248,255,255);//Branco Meio 30
+            filledCircleRGBA(ren, z,w, 20, 220,20,60,255); //Vermelho Meio 20
+            filledCircleRGBA(ren, z,w, 10, 240,248,255,255); //Branco Meio 10
 
             filledCircleRGBA(ren, x,y,4, 0,255,255,255); //Bullseye Cima
-            filledCircleRGBA(ren, z,w,4, 0,255,255,255); //Bullseye Baixo
+            filledCircleRGBA(ren, z,w,4, 0,255,255,255); //Bullseye Meio
         
             SDL_RenderPresent(ren);
         }
-          
-        SDL_Delay(500);
+         
+        x += 5 * v;
+        //y += 0;
         
-        x += 5;
-        y += 0;
+        z += 5 * v*(-1);
+        //w -= 0;
+
+        if (x <= 39 || x >= 197){    
+            v = v*(-1);
+        }
         
-        z -= 5;
-        w -= 0;
-        
+        SDL_RenderPresent(ren);
+        SDL_Delay(500);    
     }
     
-
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
