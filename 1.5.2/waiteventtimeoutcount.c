542480f8b3a8a169fbcd73829f7efb61712bd0fe
Mariana <57501545+wari99@users.noreply.github.com>
2022-12-04 22:37:56 -0300

    Update waiteventtimeoutcount.c

--- 1.5.2/waiteventtimeoutcount.c
+++ 1.5.2/waiteventtimeoutcount.c
@@ -1,15 +1,22 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
-int continua = 500;
-int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
-    if (SDL_WaitEventTimeout(evt, continua)){
-    	continua -= (SDL_GetTicks() - *ms);
-    	return 1;
-    } else {
-    	continua = 500;
-    	return 0;
-    }
+int AUX_WaitEventTimeoutCount (SDL_Event* evt, Uint32* ms){
+	Uint32 antes = SDL_GetTicks();
+	Uint32 depois = 0;
+	int isevt = SDL_WaitEventTimeout(evt, *ms);
+	if(isevt){
+		depois = (SDL_GetTicks() - antes);
+		if(*ms < depois) depois = *ms;
+		*ms -= depois;		
+	}
+	else{
+		*ms = 500;
+		
+	}
+	return isevt;
 }
+
 
 int main (int argc, char* args[]){
     /* INICIALIZACAO */
@@ -17,63 +24,88 @@ int main (int argc, char* args[]){
     SDL_Window* win = SDL_CreateWindow("WaitEventTimeout",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
-                         250, 200, SDL_WINDOW_SHOWN
+                         300, 300, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-    /* EXECUÇÃO */
-    
-    SDL_Rect r = { 10,10, 10,10 };
 
-    while (1) {
-    
+    int espera = 500;
+    SDL_Rect r1 = { 40,20, 10,10 };
+    SDL_Rect r2 = { 80,40, 20,20 };
+    SDL_Rect r3 = { 50,100, 30,30 };
+    SDL_Event evt;
+    bool continua = true;
+    int x, y;
+	/* EXECUÇÃO */
+    while (continua) {
+            	
         SDL_SetRenderDrawColor(ren, 0x0, 0x0, 0x0,0x00);
         SDL_RenderClear(ren);
         
-        SDL_Rect r0 = {10,10, 230,180};
-        SDL_SetRenderDrawColor(ren, 0x94, 0x0, 0xD3, 0x00);
+        SDL_SetRenderDrawColor(ren, 0x4B, 0x0, 0x82, 0x00);
+        SDL_Rect r0 = {10,10, 280,280};
         SDL_RenderFillRect(ren, &r0);
         
-        SDL_SetRenderDrawColor(ren, 0xFF, 0xFA, 0xF0,0x00);
-        SDL_RenderFillRect(ren, &r);
+        SDL_SetRenderDrawColor(ren, 0xFF, 0x0, 0xFF,0x00);
+        SDL_RenderFillRect(ren, &r1);
+        
+        SDL_SetRenderDrawColor(ren, 0x93, 0x70, 0xDB, 0x00);
+        SDL_RenderFillRect(ren, &r2);
+        
+        SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF,0x00);
+        SDL_RenderFillRect(ren, &r3);
+        
         SDL_RenderPresent(ren);
 
-        SDL_Event evt;
+       
         
-        Uint32 time = SDL_GetTicks();
-        int isevt = AUX_WaitEventTimeoutCount(&evt, &time);
+        int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
         
-        if (isevt){
-            if (evt.type == SDL_KEYDOWN) {
-                switch (evt.key.keysym.sym) {
-                    case SDLK_UP:
-                        r.y -= 5;
-                        break;
-                    case SDLK_DOWN:
-                        r.y += 5;
-                        break;
-                    case SDLK_LEFT:
-                        r.x -= 5;
-                        break;
-                    case SDLK_RIGHT:
-                        r.x += 5;
-                        break;
-                }
-            }
-            
-            else if (evt.type == SDL_QUIT){
-                break;
-            }
-        } 
+       if (isevt) {
+            switch (evt.type ) {
+                case SDL_MOUSEMOTION:
+                	SDL_GetMouseState(&x, &y);
+                	r3.x = x;
+                	r3.y = y;
+                case SDL_KEYDOWN:
+                	switch (evt.key.keysym.sym) {
+		                case SDLK_UP:
+		                    r1.y -= 5;
+		                    break;
+		                case SDLK_DOWN:
+		                    r1.y += 5;
+		                    break;
+		                case SDLK_LEFT:
+		                    r1.x -= 5;
+		                    break;
+		                case SDLK_RIGHT:
+		                    r1.x += 5;
+		                    break;
+		            }       
+		    }
+        }
         
         else {
-            r.x += 2;
-            r.y += 2;
+            espera = 300;
+            r2.x += 3;
+            r2.y += 3;
+            SDL_RenderPresent(ren);
         }
+        
+        /* FINALIZAÇÃO */
+        switch (evt.type ) {
+            case SDL_QUIT:
+            	continua = false;
+            	break;
+            case SDL_KEYDOWN:
+            	if(evt.key.keysym.sym == SDLK_F4){
+            		if(evt.key.keysym.mod == KMOD_LALT){
+		    		continua = false;
+		    		break;	
+            			}		
+			}
+        }
+        
     }
-
-    /* FINALIZACAO */
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+    
 }
