5d3b5a3a7fb7a3d2a459f0b642f5959f55afbdf1
Mariana <57501545+wari99@users.noreply.github.com>
2022-12-13 16:32:54 -0300

    Update recorte.c

--- 1.7/recorte.c
+++ 1.7/recorte.c
@@ -20,69 +20,69 @@ int main (int argc, char* args[])
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(0);
-    SDL_Window* win = SDL_CreateWindow("Recorte",
+    SDL_Window* win = SDL_CreateWindow("uno reverse card flip",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
-                         300, 110, SDL_WINDOW_SHOWN
+                         350, 130, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
     SDL_Texture* img = IMG_LoadTexture(ren, "cardflip.png");
     assert(img != NULL);
 
     /* EXECUÇÃO */
-    int isup = 1;
-    int x = -100;
-    int running = 1, espera = 500;
- 	SDL_Event evt;
-    
-    int cont = 0;
+    SDL_Event evt;
+    int running = 1, isup = 1, x = -50, espera = 100;
+
     while (running) {
-    	Uint32 antes = SDL_GetTicks();
-		int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
-		
         SDL_SetRenderDrawColor(ren, 254,221,2,255);
         SDL_RenderClear(ren);
         
-        SDL_Rect r = { x,0, 110,110 };
-        SDL_Rect c;
-
- 		if(isevt){       
-		    if(evt.type == SDL_QUIT){
-		    	running = 0;
-		    	break;
-		    }
-    	}
-/*
-    		c = (SDL_Rect) {0,0, 110,110}; 
-    		 		
-    		c = (SDL_Rect) {110,0, 100,110};
-    		
-    		c = (SDL_Rect) {220,0, 300, 110};*/
-    	
-    	printf("\n*%d*", isup);
-    
-    	if(isup < 20){
-    		if(isup ==1 || isup == 7 || isup == 12) c = (SDL_Rect) {0,0, 110,110};
-    		if(isup ==2 || isup == 8 || isup == 13) c = (SDL_Rect) {110,0, 110,100};
-    		if(isup ==3 || isup == 9 || isup == 14) c = (SDL_Rect) {220,0,110,110};
-    		if(isup ==4 || isup == 10 || isup == 15) c = (SDL_Rect) {330,0,110,110};    		
-    		if(isup ==5 || isup == 11 || isup == 16) c = (SDL_Rect) {440,0,110,110};    		
-    		if(isup ==6 || isup == 12 || isup == 17) c = (SDL_Rect) {550,0,110,110}; 
-    		isup++;
-        }
-    	else{
-    		break;
-    	}
-    	/*
-    	switch(isup){
-    		case
-    	}*/
-   
-        SDL_RenderCopy(ren, img, &c, &r);
-        SDL_RenderPresent(ren);
-        SDL_Delay(200);
- //       x = (x + 10) % 400;
- 		x = (x + 25) % 400;
-        //isup = !isup;
- 		
+        SDL_Rect r = { x,0,110,110 };
+        SDL_Rect corte;
+        
+        //int isevt = AUX_WaitEventTimeoutCount(&evt,&espera);
+               
+        if(AUX_WaitEventTimeoutCount(&evt,&espera)){       	
+			switch (evt.type){
+				case SDL_QUIT:
+					running = 0;
+					break;		
+			}
+		}
+		else{   
+			switch(isup){
+					case 1:
+						corte = (SDL_Rect) {0,0, 110,110};
+						break;
+					case 2:
+						corte = (SDL_Rect) {110,0, 110,100};
+						break;
+					case 3:
+						corte = (SDL_Rect) {220,0,110,110};
+						break;
+					case 4:
+						corte = (SDL_Rect) {330,0,110,110}; 
+						break;
+					case 5:
+						corte = (SDL_Rect) {440,0,110,110}; 
+						break;
+					case 6:
+						corte = (SDL_Rect) {550,0,110,110};   		
+						break;
+			}isup++;
+			if(isup == 6) isup = 1;
+	
+			SDL_RenderCopy(ren, img, &corte, &r);
+			SDL_RenderPresent(ren);
+			espera = 100;
+			
+			x = (x + 10) % 350;
+		}
     }
+
+    /* FINALIZACAO */
+    SDL_DestroyTexture(img);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
