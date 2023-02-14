553ee4a60b6670e83d509472797dc644f3b9112b
Mariana <57501545+wari99@users.noreply.github.com>
2023-02-14 15:23:39 -0300

    Update jogo.c

--- Projeto/jogo.c
+++ Projeto/jogo.c
@@ -81,15 +81,14 @@ void constroi(SDL_Rect setor[]){
 	setor[5] = (SDL_Rect) {211,152,160,86}; //Canto esquerdo
 }
 
-void calculaXmov(short int *xMov, dadosBarra barra1){ //MUDEI
+void calculaXmov(short int *xMov, dadosBarra barra1){ 
 	int aux = barra1.bola.rect.x + (barra1.bola.rect.w)/2;
+	
 	if(aux >= 220 && aux <= 250) *xMov = -8;
 	else if(aux > 250 && aux <= 310) *xMov = -6;
 	else if(aux > 310 && aux <= 410) *xMov = -4;
 	else if(aux > 410 && aux <= 440) *xMov = -2;
-	
 	else if(aux > 440 && aux <= 470) *xMov = 0;
-	
 	else if(aux > 470 && aux <= 500) *xMov = 2;
 	else if(aux > 500 && aux <= 530) *xMov = 4;
 	else if(aux > 530 && aux <= 560) *xMov = 6;
@@ -102,61 +101,42 @@ void calculaForca(short int *forca, dadosBarra barra2){
 	if(aux >= 87 && aux <= 117) *forca = 100;
 	else if(aux > 117 && aux <= 147) *forca = 90;
 	else if(aux > 147 && aux <= 177) *forca = 80;
-	
 	else if(aux > 177 && aux <= 207) *forca = 65;
-	
 	else if(aux > 207 && aux <= 237) *forca = 55;
 	else if(aux > 237 && aux <= 267) *forca = 50;
 	else if(aux > 267 && aux <= 297) *forca = 40;
 	else if(aux > 297 && aux <= 327) *forca = 30;
 }
 
-void calculaStatusBall(int *statusBall, dadosBola bola){ //0E 1B 2C 3A 4F 5D  //MUDEI
+void calculaStatusBall(short int *statusBall, dadosBola bola){ 
 	int auxX = bola.rect.x - (bola.rect.w)/2;
 	int auxY = bola.rect.y - (bola.rect.h)/2;
-	if(auxX>=211 && auxX<=400 && auxY>=66 && auxY<=152){ //(+30) 211/400/66/152 
-		//printf("\nEspaco A!");
-		*statusBall = 3;
-	}
-	else if(auxX>=401 && auxX <= 500 && auxY>= 66 && auxY<= 152){ //(+30)(-30) 401/500/66/152
-		//printf("\nEspaco B!");
-		*statusBall = 1;
-	} //
-	else if(auxX>=401 && auxX<=691 && auxY>=66 && auxY<=152){ // 400/691/66/152
-		//printf("\nEspaco C!");
-		*statusBall = 2;
-	}
-
-	else if(auxX >=211 && auxX<=400 && auxY>=152 && auxY<=238){ // 211/400/152/238
-		//printf("\nEspaco D!");
-		*statusBall = 5;
-	}
-	else if(auxX>=401 && auxX <= 500 && auxY>= 152 && auxY<= 238){ // 401/500/152/238
-		//printf("\nEspaco E!");
-		*statusBall = 0;
-	}
-	else if(auxX>=400 && auxX<=691 && auxY>=152 && auxY<=238){ //  400/691/152/238/
-		//printf("\nEspaco F!");
-		*statusBall = 4;
-	}
-	else{
-		//printf("\nFora!");
-		*statusBall = 6;
-	}	
+	
+	if(auxX>=211 && auxX<=400 && auxY>=66 && auxY<=152) *statusBall = 3;
+	else if(auxX>=401 && auxX <= 500 && auxY>= 66 && auxY<= 152)*statusBall = 1;
+	else if(auxX>=401 && auxX<=691 && auxY>=66 && auxY<=152) *statusBall = 2;
+	else if(auxX >=211 && auxX<=400 && auxY>=152 && auxY<=238) *statusBall = 5;	
+	else if(auxX>=401 && auxX <= 500 && auxY>= 152 && auxY<= 238) *statusBall = 0;
+	else if(auxX>=400 && auxX<=691 && auxY>=152 && auxY<=238) *statusBall = 4;
+	else *statusBall = 6;	
 }
 
 void constroiPont(SDL_Renderer *ren,dadosPontuacao* pontuacao, int x,int y, char *endereco){
 	pontuacao->n = 0;
 	pontuacao->rect = (SDL_Rect) {x,y,240,40};
+	
 	int aux = pontuacao->rect.x + 40;
+	
 	pontuacao->i = 0;
     pontuacao->texture = IMG_LoadTexture(ren, endereco);
     pontuacao->valor = 0;
+    
 	for(int i = 0; i<5;i++){
 		pontuacao->vetPontos[i].img = NULL;
 	 	pontuacao->vetPontos[i].state = false;
 	 	SDL_Rect recAux = {aux,y,40,40};
 	 	pontuacao->vetPontos[i].r = recAux;
+	 	
 	 	aux+=40;
     }
 }
@@ -183,40 +163,40 @@ void mudaCor(SDL_Renderer* ren,SDL_Surface* listaS[],SDL_Texture* listaT[],SDL_C
 }
 
 void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
+
 	//cria torcida
 	dadosTorcida torcida;
 	torcida.texture = IMG_LoadTexture(ren, "torcidaarquibancada1.png");
 	torcida.state = up;
 	torcida.aux = 0;
+	
 	//cria goleiro
 	dadosGoleiro goleiro;
-	goleiro.rect = (SDL_Rect) {350, 100, 200,170}; //MUDEI
+	goleiro.rect = (SDL_Rect) {350, 100, 200,170}; 
 	goleiro.corte = (SDL_Rect) {0,0,200,170};
 	goleiro.texture = IMG_LoadTexture(ren, "goleiros200x170.png");	
-	//goleiro.texture = IMG_LoadTexture(ren, "quadrado.png");
 	goleiro.state = esperando;
 	goleiro.aux = 0;
-	
 
 	//cria barra1
 	dadosBarra barra1;
-	barra1.rect = (SDL_Rect) {320,250,270,140}; //MUDEI 320
+	barra1.rect = (SDL_Rect) {320,250,270,140}; 
 	barra1.texture = IMG_LoadTexture(ren, "barra.png");
 	barra1.state = on;
-	barra1.bola.rect = (SDL_Rect) {300, 300, 40,40}; //MUDEI 300
+	barra1.bola.rect = (SDL_Rect) {300, 300, 40,40}; 
 	barra1.bola.texture = IMG_LoadTexture(ren, "bola.png");
 	
 	//cria barra2
 	dadosBarra barra2;
-	barra2.rect = (SDL_Rect) {715,87,140,240}; //MUDEI 715
+	barra2.rect = (SDL_Rect) {715,87,140,240};
 	barra2.texture = IMG_LoadTexture(ren, "barra2.png");
 	barra2.state = off;
-	barra2.bola.rect = (SDL_Rect) {765, 150, 40,40}; //MUDEI 770
+	barra2.bola.rect = (SDL_Rect) {765, 150, 40,40}; 
 	barra2.bola.texture = IMG_LoadTexture(ren, "bola.png");
 
 	//cria bola
 	dadosBola bola;
-	bola.rect = (SDL_Rect) {420, 300, 40,40}; //MUDEI 420
+	bola.rect = (SDL_Rect) {420, 300, 40,40}; 
 	bola.texture = IMG_LoadTexture(ren, "bola.png");
 	bola.state = parada;
 	bola.aux = 0;
@@ -228,69 +208,63 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 	dadosPlayer player;
 	player.state = vezBatedor;
 	player.pontuacao = 0;
+	
 	//setores do gol
 	struct SDL_Rect setor[6];
 	constroi(setor);
 
 	SDL_Point mouse = {0,0};
 	
-	int espera = 200;
-	short int forca = 1;
-	short int xMov = 0;
+	short int statusBall = -1, isMoving = 1, cont, forca = 1, xMov = 0; 
+	int x,y, espera = 200; 	
 	bool aguardando = false;
-	int statusBall = -1, isMoving = 1; 
-	int x,y,dx,dy; 
+	
 	Uint32 antes = 0;
-	short int cont;
 	unsigned short int posBola = 6;
 	
 	//cria pontuacao1
-	
 	dadosPontuacao pontuacao1;
 	constroiPont(ren,&pontuacao1,0,260,"timeA.png");
 	dadosPontuacao pontuacao2;
 	constroiPont(ren,&pontuacao2,0,305,"timeB.png");
 	
     /* EXECUÇÃO */
-    
 	assert(imgGol != NULL);	
 	assert(imgBola != NULL);
-
 				
-	int aguardandoAux = 1;
-	int contadorX = 140;
-	int contadorY = 87;
-	
-	SDL_Rect rBX = {171,66,160,86}; // Bola Eixo X
+	int contadorX = 140, contadorY = 87, aguardandoAux = 1, i = 0;
 	bool setorSelecionado = false;
-	short int setorAux = -1;
-	short int bolaaux = 1;
-	short int bolaauxY = 1;
-	int i = 0;
+	short int setorAux = -1, bolaaux = 1, bolaauxY = 1;
 	
+	SDL_Rect rBX = {171,66,160,86};	
 	SDL_Rect rectGol = {241,96,420,142};
+	
 	while(*screen == jogo){
 			espera = MAX(espera - (int)(SDL_GetTicks() - antes), 0);
 		  	SDL_Event evt; 
 		  	antes = SDL_GetTicks();
+		  	
 			SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0x00);
 			SDL_RenderClear(ren);
 			SDL_RenderCopy(ren, torcida.texture, NULL, NULL);//FUNDO DE TELA
 			SDL_RenderCopy(ren, imgGol, NULL, NULL);//FUNDO DE TELA
+			
             chamaPont(ren,pontuacao1);
 			chamaPont(ren,pontuacao2);
+			
 	    	SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF,0x00);
+	    	
 			if(goleiro.state == esperando){
 				if(player.state == vezGoleiro){						
 					SDL_SetRenderDrawColor(ren, 255,0,0,255);	
-					
 					SDL_RenderDrawRect (ren, &setor[0]);
 					SDL_RenderDrawRect (ren, &setor[1]);
 					SDL_RenderDrawRect (ren, &setor[2]);				
 					SDL_RenderDrawRect (ren, &setor[3]);
 					SDL_RenderDrawRect (ren, &setor[4]);
 					SDL_RenderDrawRect (ren, &setor[5]);
-				if(setorSelecionado ){
+					
+				if(setorSelecionado){
 					SDL_SetRenderDrawColor(ren, 255,255,255,255);	
 					SDL_RenderFillRect (ren, &setor[setorAux]);
 					}
@@ -298,7 +272,7 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 			}
 			SDL_RenderCopy(ren, goleiro.texture, &goleiro.corte, &goleiro.rect);
 			
-			if(player.state  == vezBatedor){
+			if(player.state == vezBatedor){
 				if(barra1.state == on){
 					SDL_RenderCopy(ren, barra1.texture, NULL, &barra1.rect);// BOLA X
 					SDL_RenderCopy(ren, barra1.bola.texture, NULL, &barra1.bola.rect);// BOLA X
@@ -312,24 +286,20 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 				SDL_RenderCopy(ren, bola.texture, NULL, &bola.rect);// BOLA do jogo
 			}
 			
-			
-			//TRATAR O GOL DO VEZ BATEDOR
+			//TRATAR O GOL DO vezBatedor
 			if(bola.aux == forca){	
 				forca = -1;		
 				calculaStatusBall(&statusBall, bola);
 				printf("\n *Status ball: %d* *goleiro.pos: %d* ",statusBall,goleiro.pos);
-				//if(statusBall == goleiro.pos){
 				if(SDL_HasIntersection(&bola.rect, &goleiro.rect)){
 					printf("Agarrou!!");
 					pontuacao1.vetPontos[pontuacao1.n].img = IMG_LoadTexture(ren, "perdeu.png");
 				}
-				//else if((statusBall >= 0 && statusBall < 6) && statusBall != goleiro.pos ){
 				else if(!SDL_HasIntersection(&bola.rect, &goleiro.rect) && SDL_HasIntersection(&bola.rect, &rectGol)){
 					printf("Gol!!!");
 					pontuacao1.vetPontos[pontuacao1.n].img = IMG_LoadTexture(ren, "acertou.png");
 					pontuacao1.valor++;
 				}
-				//else if(statusBall == 6){ 
 				else if(!SDL_HasIntersection(&bola.rect, &rectGol)){
 					printf("Fora!!!");
 					pontuacao1.vetPontos[pontuacao1.n].img = IMG_LoadTexture(ren, "perdeu.png");
@@ -337,14 +307,14 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 				aguardando = true;
 				(pontuacao1.n)++;
 			}
-			//TRATAR O GOL VEZ GOLEIRO
-			if(player.state == vezGoleiro && goleiro.state == agarrando){ //Vendo se outrogoleiro conseguiu agarrar ou tomou gol, tira a animacao de agarrar 
+			
+			//TRATAR O GOL vezGoleiro
+			if(player.state == vezGoleiro && goleiro.state == agarrando){
 				aguardando = true;
 				bola.state = parada;
 			}
 			
 			SDL_RenderPresent(ren);
-			//TRATAR O VENCEDOR
 			
 		  	int isevt = AUX_WaitEventTimeoutCount(&evt,&espera);    
 		    if(isevt){ 	
@@ -362,13 +332,11 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 						SDL_GetMouseState(&mouse.x, &mouse.y);
 						if(evt.button.button == SDL_BUTTON_LEFT){
 							if(evt.button.state==SDL_RELEASED){									
-								if(barra2.state == on) barra2.state = off;	
-										
+								if(barra2.state == on) barra2.state = off;			
 							 	if(barra1.state == on){
 							 		barra1.state = off;
 							 		barra2.state = on;
 							 	}
-
 								if(player.state == vezGoleiro){
 									SDL_GetMouseState(&mouse.x, &mouse.y);
 									if(SDL_PointInRect(&mouse, &setor[setorAux]) && goleiro.state == esperando){
@@ -422,7 +390,7 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 			   			calculaXmov(&xMov, barra1);
 			   			calculaForca(&forca,barra2); 
 			   		}
-			   		if(bola.state == girando && bola.aux <= forca){// && bola.aux <= forca
+			   		if(bola.state == girando && bola.aux <= forca){ //Até a bola alcançar a força correspondente (movimentacoes)
 						bola.rect.x += xMov;
 						bola.rect.y -= 3;
 						bola.aux++;
@@ -430,8 +398,7 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 				}				
 				
 				else if(player.state == vezGoleiro && goleiro.state == agarrando){
-				
-					switch(posBola){ // Indo para o mesmo lugar que o goleiro
+					switch(posBola){ 
 						case 0: //Meio baixo
 							if(bola.rect.y >= 205) bola.rect.y -= 3;
 							if(bola.rect.y < 205) bola.state = parada;
@@ -484,7 +451,7 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 						aguardando = false;
 						player.state = vezBatedor;
 						bola.state = parada;
-						bola.rect.x = 420; //MUDEI
+						bola.rect.x = 420;
 						bola.rect.y = 300;
 						if(posBola == goleiro.pos && bola.state == parada){
 							pontuacao2.vetPontos[pontuacao2.n].img = IMG_LoadTexture(ren, "perdeu.png");
@@ -496,7 +463,7 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 						(pontuacao2.n)++;
 						goleiro.texture = IMG_LoadTexture(ren, "goleiros200x170.png");
 						goleiro.corte = (SDL_Rect) {0,0,200,170};
-						goleiro.rect.x = 350; //MUDEI
+						goleiro.rect.x = 350;
 						goleiro.rect.y = 100;
 						goleiro.state = esperando;
 						aguardandoAux = 0;
@@ -532,19 +499,19 @@ void rodaJogo(SDL_Renderer* ren, int *screen,int *vencedor){
 						case 1: //Meio cima
 							if(goleiro.rect.y >= 50) goleiro.rect.y -= 3;
 							break;
-						case 2: //Angulo direito //MUDEI
+						case 2: //Angulo direito
 							if(goleiro.rect.x <= 500) goleiro.rect.x += 3;
 							if(goleiro.rect.y >= 50) goleiro.rect.y -= 1;
 							break;
-						case 3: //Angulo esquerdo //MUDEI
+						case 3: //Angulo esquerdo 
 							if(goleiro.rect.x >= 200) goleiro.rect.x -= 3;
 							if(goleiro.rect.y >= 50) goleiro.rect.y -= 1;
 							break;
-						case 4: //Canto direito	 //MUDEI
+						case 4: //Canto direito	 
 							if(goleiro.rect.x <= 480) goleiro.rect.x += 3;
 							if(goleiro.rect.y >= 70) goleiro.rect.y -= 1;
 							break;
-						case 5: //Canto esquerdo //MUDEI
+						case 5: //Canto esquerdo
 							if(goleiro.rect.x >= 210) goleiro.rect.x -= 3;
 							if(goleiro.rect.y >= 70) goleiro.rect.y -= 1;
 							break;
@@ -633,10 +600,6 @@ void chamaMenu(SDL_Renderer* ren,int *screen){
 						if(SDL_PointInRect(&mouse,&recPlay)){
 					   		mudaCor(ren,listaSurfaceText,listaTextureText,focus,0,"Play",ourFont);
 					   	}
-
-					   /*	else if(SDL_PointInRect(&mouse,&recAbout)){
-					   		mudaCor(ren,listaSurfaceText,listaTextureText,focus,1,"About",ourFont);					   	
-					   	}*/
 					   	
 					   	else if(SDL_PointInRect(&mouse,&recQuit)){
 					   		mudaCor(ren,listaSurfaceText,listaTextureText,focus,2,"Quit",ourFont);
@@ -644,7 +607,6 @@ void chamaMenu(SDL_Renderer* ren,int *screen){
 					   	}
 					   	else{
 					   		mudaCor(ren,listaSurfaceText,listaTextureText,padrao,0,"Play",ourFont);
-					   		//mudaCor(ren,listaSurfaceText,listaTextureText,padrao,1,"About",ourFont);					   	
 					   		mudaCor(ren,listaSurfaceText,listaTextureText,padrao,2,"Quit",ourFont);
 							selecionado = false;
 					   	}
@@ -670,7 +632,6 @@ void chamaMenu(SDL_Renderer* ren,int *screen){
 		}
 		SDL_RenderCopy(ren,bgmenu,NULL,NULL);
 		SDL_RenderCopy(ren,listaTextureText[0],NULL,&recPlay);
-		//SDL_RenderCopy(ren,listaTextureText[1],NULL,&recAbout);
 		SDL_RenderCopy(ren,listaTextureText[2],NULL,&recQuit);
 		SDL_RenderPresent(ren);
 	}	
@@ -760,9 +721,9 @@ int main (int argc, char* args[]){
             	rodaTelaFinal(ren,&vencedor,&screen);
         } 
 	}
+	
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
-	
