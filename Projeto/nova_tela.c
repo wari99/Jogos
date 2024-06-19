86bc3fc6623af271b0d1d76c341b8612b978ae2f
Mariana <57501545+wari99@users.noreply.github.com>
2024-06-19 20:37:54 -0300

    Update nova_tela.c
    V1: Acao da bola ao ser clicada e gravidade

--- Projeto/nova_tela.c
+++ Projeto/nova_tela.c
@@ -2,58 +2,62 @@
 #include <SDL2/SDL.h>
 #include <math.h>
 
-const int dimensao_x = 800;
-const int dimensao_y = 600;
+const int largura_tela = 800;
+const int altura_tela = 600;
 
 SDL_Window* window = NULL;
 SDL_Renderer* ren = NULL;
 
-typedef struct{
-    int x, y; //posicao	
-    int raio; //raio da bola
-	int velocidade_y; //velocidade eixo y
+typedef struct {
+    int x, y;
+    int raio;
+    float velocidade_Y;
 	
-    Uint8 r, g, b, a; //cores
+    Uint8 r, g, b, a;
 } Bola;
 
-Bola bola = {largura_tela/2, altura_tela/2, 50, 0, 255, 0, 0, 255};
-const int gravidade = 1;
+Bola bola = {largura_tela / 2, altura_tela / 2, 70, 0, 255, 0, 0, 255};
+const float GRAVIDADE_CONST = 0.5;
 
-void desenharBola(const Bola* bola) {
+void drawBola(const Bola* bola) {
     filledCircleRGBA(ren, bola->x, bola->y, bola->raio, bola->r, bola->g, bola->b, bola->a);
 }
 
 void movimentoBola(Bola* bola) {
-    bola->velocidade_y += gravidade;
-    bola->y += bola->velocidade_y;
+    bola->velocidade_Y += GRAVIDADE_CONST;
+    bola->y += bola->velocidade_Y;
 
-    // Colisão com a parte inferior da tela
     if (bola->y + bola->raio > altura_tela) {
         bola->y = altura_tela - bola->raio;
-        bola->velocidade_y = 0;
+        bola->velocidade_Y = 0;
     }
 }
 
 void acaoClique(int mouseX, int mouseY) {
     int dx = mouseX - bola.x;
     int dy = mouseY - bola.y;
-    int distanciaFormula = dx * dx + dy * dy; //calculando distancia entre clique e centro da bola
-    
-	if (distanciaFormula <= bola.raio * bola.raio) {
-        if (mouseY < bola.y - bola.raio / 3) bola.velocidade_y = 10;  //parte superior
-		else if (mouseY > bola.y + bola.raio / 3) bola.velocidade_y = -10; // parte inferior
-		else bola.velocidade_y = 5; // meio
+	
+    int distanciaFormula = dx * dx + dy * dy;
+	
+    if (distanciaFormula <= bola.raio * bola.raio) {
+        if (mouseY < bola.y - bola.raio / 3) {
+            bola.velocidade_Y = -12; 
+        } else if (mouseY > bola.y + bola.raio / 3) {
+            bola.velocidade_Y = -17; 
+        } else {
+            bola.velocidade_Y = -20; 
+        }
     }
 }
 
 void mainLoop() {
     SDL_Event evt;
-    int in_game = 1;
+    int running = 1;
 
-    while (in_game) {
+    while (running) {
         while (SDL_PollEvent(&evt)) {
             if (evt.type == SDL_QUIT) {
-                in_game = 0;
+                running = 0;
             } else if (evt.type == SDL_MOUSEBUTTONDOWN) {
                 acaoClique(evt.button.x, evt.button.y);
             }
@@ -63,11 +67,11 @@ void mainLoop() {
         SDL_RenderClear(ren);
 
         movimentoBola(&bola);
-        desenharBola(&bola);
+        drawBola(&bola);
 
         SDL_RenderPresent(ren);
 
-        SDL_Delay(16); // 
+        SDL_Delay(16); //retirar dps
     }
 }
 
@@ -84,7 +88,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    ren = SDL_Createren(window, -1, SDL_ren_ACCELERATED);
+    ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!ren) {
         printf("Erro ao criar ren: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
