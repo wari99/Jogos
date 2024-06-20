cb34e671db3a341bfc3fe9ac752aa6c53daf7c3b
Mariana <57501545+wari99@users.noreply.github.com>
2024-06-19 21:57:50 -0300

    V4
    V4: Velocidade inicial, gravidade, velocidade horizontal, colisão com paredes, deltaTime, adicionando eixo X

--- Projeto/nova_tela.c
+++ Projeto/nova_tela.c
@@ -2,48 +2,64 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <math.h>
+#define MAX(x,y) (((x) > (y)) ? (x) : (y))
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 SDL_Window* window = NULL;
-SDL_Renderer* ren = NULL;
+SDL_Renderer* renderer = NULL;
 TTF_Font* font = NULL; 
 
 typedef struct {
     int x, y;
     int raio;
-    float velocidade_Y;
+	
+    float velocidade_Y;	
+    float velocidade_X; //adiciona a velocidade pro eixo X
 	
     Uint8 r, g, b, a;
-	
     int encostaChao; 
 } Bola;
 
-Bola bola = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 70, 0, 255, 0, 0, 255, 1}; // Inicia a bola no chão
-const float GRAVIDADE = 0.5;
-int cont = 0;
+Bola bola = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 70, 0, 0, 255, 0, 0, 255, 1}; 
+
+//Ajuste de Gravidade/Vel inicial/Vel Horzioontal
+const float GRAVIDADE = 900.0f; 
+const float VEL_INICIAL = 500.0f; 
+const float VEL_HORIZONTAL = 200.0f; 
 
 void desenhoBola(const Bola* bola) {
-    filledCircleRGBA(ren, bola->x, bola->y, bola->raio, bola->r, bola->g, bola->b, bola->a);
+    filledCircleRGBA(renderer, bola->x, bola->y, bola->raio, bola->r, bola->g, bola->b, bola->a);
 }
 
-void movimentoBola(Bola* bola) {
-    bola->velocidade_Y += GRAVIDADE;
-    bola->y += bola->velocidade_Y;
+void movimentoBola(Bola* bola, Uint32 deltaTime) { // 1000px/seg
+    bola->velocidade_Y += GRAVIDADE * deltaTime / 1000.0f; 
+    bola->y += bola->velocidade_Y * deltaTime / 1000.0f;
+    bola->x += bola->velocidade_X * deltaTime / 1000.0f;
 
-	//colidir com o interior da tela
+    //colidir com a parte iferior
     if (bola->y + bola->raio > SCREEN_HEIGHT) {
         bola->y = SCREEN_HEIGHT - bola->raio;
         bola->velocidade_Y = 0;
-        bola->encostaChao = 1; 
+        bola->encostaChao = 1; //bola no chao
+		
+        cont = 0; //reseta cont
+		
+    } else if (bola->y - bola->raio < 0) {
+        bola->y = bola->raio;
+        bola->velocidade_Y = 0;
     } else {
-        bola->encostaChao = 0;
+        bola->encostaChao = 0; //bola esta no ar 
     }
 
-	//se encosta no chao reseta 
-    if (bola->encostaChao && cont > 0) {
-        cont = 0;
+    //colidir com laterais
+    if (bola->x + bola->raio > SCREEN_WIDTH) {
+        bola->x = SCREEN_WIDTH - bola->raio;
+        bola->velocidade_X = -bola->velocidade_X; //inverter velocidade horizontal
+    } else if (bola->x - bola->raio < 0) {
+        bola->x = bola->raio;
+        bola->velocidade_X = -bola->velocidade_X; //inverter velocidade horizontal
     }
 }
 
@@ -51,23 +67,30 @@ void acaoClique(int mouseX, int mouseY) {
     int dx = mouseX - bola.x;
     int dy = mouseY - bola.y;
 	
-    float distancia = sqrt(dx * dx + dy * dy); //dist entre o ponto do clique e o centro da bola
-    
-	if (distancia <= bola.raio && !bola.encostaChao) {
+    float distanciaFormula = dx * dx + dy * dy;
+
+    if (distanciaFormula <= bola.raio * bola.raio) {
         if (mouseY < bola.y - bola.raio / 3) {
-            bola.velocidade_Y = -12; 
+            bola.velocidade_Y = -VEL_INICIAL * 0.5;  // parte superior sobe com vel menor
         } else if (mouseY > bola.y + bola.raio / 3) {
-            bola.velocidade_Y = -17; 
+            bola.velocidade_Y = -VEL_INICIAL * 0.8; //parte inferior vel mais alta
         } else {
-            bola.velocidade_Y = -20;
+            bola.velocidade_Y = -VEL_INICIAL; //parte media vel media 
         }
-        cont++; 
+
+        if (mouseX < bola.x) { inverter velocidade horizontal
+            bola.velocidade_X = VEL_HORIZONTAL; //click lado esquerdo = vai pra direita
+        } else if (mouseX > bola.x) {
+            bola.velocidade_X = -VEL_HORIZONTAL; //click lado direito = vai pra esquerda
+        }
+
+        cont++; //se a bola n tiver no chao ++ 
     }
 }
 
-void renderizarTexto(const char* text, int x, int y, SDL_Color textColor) {
+void renderText(const char* text, int x, int y, SDL_Color textColor) {
     SDL_Surface* surface = TTF_RenderText_Solid(font, text, textColor);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(ren, surface);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
     int texW = 0;
@@ -75,39 +98,63 @@ void renderizarTexto(const char* text, int x, int y, SDL_Color textColor) {
     SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
 
     SDL_Rect dstRect = { x, y, texW, texH };
-    SDL_RenderCopy(ren, texture, NULL, &dstRect);
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
     SDL_DestroyTexture(texture);
 }
 
+int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms) {
+    Uint32 antes = SDL_GetTicks();
+    SDL_FlushEvent(SDL_MOUSEMOTION);
+    if (SDL_WaitEventTimeout(evt, *ms)) {
+        *ms = MAX(0, *ms - (int)(SDL_GetTicks() - antes));
+        return 1;
+    }
+    return 0;
+}
+
 void mainLoop() {
-    SDL_Event evt;
+    Uint32 startTime = SDL_GetTicks(); 
+    Uint32 deltaTime = 0; 
+    Uint32 frameStart = startTime; 
+
     int in_game = 1;
 
     while (in_game) {
-        while (SDL_PollEvent(&evt)) {
-            if (evt.type == SDL_QUIT) {
-                in_game = 0;
-            } else if (evt.type == SDL_MOUSEBUTTONDOWN) {
-                acaoClique(evt.button.x, evt.button.y);
-            }
-        }
+        Uint32 frameTime = SDL_GetTicks() - frameStart;
+        frameStart = SDL_GetTicks();
 
-        SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
 
-        movimentoBola(&bola);
+        //att a bola com base no  deltaTime
+        movimentoBola(&bola, frameTime);
         desenhoBola(&bola);
+
 
         SDL_Color textColor = { 0, 0, 0, 255 };
         char textoContador[50];
-		
         snprintf(textoContador, sizeof(textoContador), "Embaixadinhas: %d", cont);
-        
-		renderizarTexto(textoContador, 10, 10, textColor);
+        renderText(textoContador, 10, 10, textColor);
 
-        SDL_RenderPresent(ren);
+        SDL_RenderPresent(renderer);
 
-        SDL_Delay(16); 
+
+        SDL_Event event;
+        Uint32 waitTime = 16; 
+        if (AUX_WaitEventTimeoutCount(&event, &waitTime)) {
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                acaoClique(event.button.x, event.button.y);
+            } else if (event.type == SDL_QUIT) {
+                in_game = 0; 
+            }
+        }
+
+        //calculo do deltaTime
+        deltaTime = SDL_GetTicks() - startTime;
+
+        if (frameTime < 16) {
+            SDL_Delay(16 - frameTime);
+        }
     }
 }
 
@@ -130,9 +177,9 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!ren) {
-        printf("Erro ao criar ren: %s\n", SDL_GetError());
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Erro ao criar renderer: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
@@ -141,7 +188,7 @@ int main(int argc, char* argv[]) {
     font = TTF_OpenFont("Mont-HeavyDEMO.otf", 24); 
     if (!font) {
         printf("Erro ao carregar fonte: %s\n", TTF_GetError());
-        SDL_DestroyRenderer(ren);
+        SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
@@ -150,7 +197,7 @@ int main(int argc, char* argv[]) {
     mainLoop();
 
     TTF_CloseFont(font);
-    SDL_DestroyRenderer(ren);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_Quit();
     SDL_Quit();
